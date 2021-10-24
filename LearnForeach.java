8a9da2b2a39667b1571e94443ed4f073deca39cf
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-24 22:11:22 +0530

    Add files via upload

---
+++ LearnForeach.java
@@ -0,0 +1,15 @@
+package week4.day2;
+
+public class LearnForeach {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 3, 4, 5, 6, 7, 8 };
+		for (int i : arr) {
+
+			System.out.println(i);
+
+		}
+
+	}
+}
