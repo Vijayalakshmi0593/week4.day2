9e2b616055b24f2baed980cf47d47a52f6ef6961
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-24 22:01:21 +0530

    Add files via upload

---
+++ ExplicitlyWaitDisappears.java
@@ -0,0 +1,34 @@
+package week4.day2;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExplicitlyWaitDisappears {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.get("http://www.leafground.com/pages/disapper.html");
+
+		driver.manage().window().maximize();
+
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20) );
+		WebElement button = driver.findElement(By.id("btn"));
+
+		wait.until(ExpectedConditions.invisibilityOf(button));
+
+		String text = driver.findElement(By.tagName("strong")).getText();
+		System.out.println("Text: " + text);
+
+	}
+
+}
