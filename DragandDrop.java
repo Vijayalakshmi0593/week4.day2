fec92474b90465874d85f10b0a28f62b9893767e
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-26 22:37:01 +0530

    Add files via upload

---
+++ DragandDrop.java
@@ -0,0 +1,41 @@
+package week4.day2;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DragandDrop {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.get("http://www.leafground.com/pages/drop.html");
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebElement Drag = driver.findElement(By.id("draggable"));
+
+		WebElement Drop = driver.findElement(By.id("droppable"));
+
+		Actions builder = new Actions(driver);
+
+		builder.dragAndDrop(Drag, Drop).perform();		
+		
+		String text = driver.findElement(By.id("droppable")).getText();
+		System.out.println(text);
+
+	}
+
+}
