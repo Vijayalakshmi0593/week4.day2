1ff7778954cfae2fb4d23a1f6e191f07670a21cf
Vijayalakshmi0593 <92051694+Vijayalakshmi0593@users.noreply.github.com>
2021-10-26 22:49:33 +0530

    Add files via upload

---
+++ LearnDoubleClick.java
@@ -0,0 +1,37 @@
+package week4.day2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LearnDoubleClick {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+
+		ChromeDriver driver = new ChromeDriver();
+
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+		driver.manage().window().maximize();
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		WebElement doubleclick = driver.findElement(By.xpath("//label[text()='Username']"));
+
+		Actions builder = new Actions(driver);
+		builder.doubleClick(doubleclick).perform();
+		
+		
+		//For right click;
+		
+		builder.contextClick().perform();
+	}
+
+}
